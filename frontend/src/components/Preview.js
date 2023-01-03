3d7a8f680dba9eaf94aaad603f6aa6421f1b37ff
Chris Diorio <chris.diorio12@gmail.com>
2023-01-03 17:39:24 -0600

    Initial preview

--- frontend/src/components/Preview.js
+++ frontend/src/components/Preview.js
@@ -1,12 +1,32 @@
 import React from "react"
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
 
 const Preview = ({ games }) => {
   return (
-    <ul>
+    <Grid container direction="column">
       {games.map((game) => {
-        return <li key={`game_${game.id}`}>{game.attributes.name}</li>
+        return (
+          <Grid container>
+            <Grid item key={`game_${game.id}`}>
+              <Typography>{game.attributes.name}</Typography>
+            </Grid>
+            <Grid item>
+              <iframe
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/episode/6KTESrWrG3iMO6EParK8Zr?utm_source=generator"
+                width="100%"
+                height="152"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              />
+            </Grid>
+          </Grid>
+        )
       })}
-    </ul>
+    </Grid>
   )
 }
 
